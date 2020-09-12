d48f29b4f08ae2a6bc243d6aba42d5a7c903fb87
Maick Souza <maick_a_s@msn.com>
2020-09-12 11:22:32 -0300

    sttyle(web): div elements #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,41 @@
 // import Head from 'next/head'
 
-import { Heading } from '@chakra-ui/core';
+import { Flex, Grid, Heading } from '@chakra-ui/core';
 
 export default function Home() {
   return (
-    <Heading>Maick Souza</Heading>
+    <Grid
+      as="main"
+      height="100vh"
+      templateColumns="1fr 480px 480px 1fr"
+      templateRows="1fr 480px 1fr"
+      templateAreas="
+        '. . . .'
+        '. logo form .'
+        '. . . .'
+      "
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
+        <img src="/rocketseat.svg" alt="Rocketseat"/>
+
+        <Heading size="2xl" lineHeight="shorter" marginTop={16}>
+          Faça seu login na plataforma
+        </Heading>        
+      </Flex>
+
+      <Flex
+        gridArea="form"
+        height="100%"
+        backgroundColor="gray.700"
+        borderRadius="md"
+        flexDir="column"
+        alignItems="stretch"
+        padding={16}
+      >
+
+      </Flex>
+    </Grid>
   )
 }
